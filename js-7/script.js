178e798bd3d755145d888fac802cc9ce37f87be0
Ognen Kirov Todorovski <todorovskiognen@gmail.com>
2026-06-02 22:00:48 +0200

    add localStorage

--- js-7/script.js
+++ js-7/script.js
@@ -1,7 +1,52 @@
 const formElement = document.getElementById("form")
 const booksTable = document.getElementById("books-table")
 
-let editRowId
+let darkMode = JSON.parse(localStorage.getItem("dark-mode"))
+
+if (darkMode === true) {
+    document.body.classList.add("dark")
+} else {
+    document.body.classList.remove("dark")
+}
+
+function toggleDarkMode() {
+    const isDark = document.body.classList.toggle("dark")
+    localStorage.setItem("dark-mode", isDark)
+}
+
+let books = JSON.parse(localStorage.getItem("books"))
+console.log(books);
+
+if (books === null) {
+    books = []
+    localStorage.setItem("books", "[]")
+}
+
+function renderBooks() {
+    const tableBody = booksTable.querySelector("tbody")
+    tableBody.innerHTML = ""
+    books.forEach(book => {
+        const row = document.createElement("tr")
+
+        row.innerHTML =
+            `
+            <td>${book.bookTitle}</td>
+            <td>${book.bookAuthor}</td>
+            <td>${book.bookYear}</td>
+            <td>${book.bookGenre}</td>
+            <td>
+                <button onclick="editRow('${book.id}')">Edit</button>
+                <button onclick="deleteRow('${book.id}')">Delete</button>
+            </td>
+        `
+
+        tableBody.appendChild(row)
+    })
+}
+
+renderBooks()
+
+let editBookId
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -11,61 +56,81 @@ formElement.addEventListener("submit", (event) => {
     const year = formElement.elements['book-year'].value
     const genre = formElement.elements['book-genre'].value
 
-    if (editRowId) {
-        const row = document.getElementById(editRowId)
+    if (editBookId) {
+        const book = books.find(book => book.id === editBookId)
+        // const row = document.getElementById(editBookId)
 
-        row.cells[0].innerText = formElement.elements['book-title'].value
-        row.cells[1].innerText = formElement.elements['book-author'].value
-        row.cells[2].innerText = formElement.elements['book-year'].value
-        row.cells[3].innerText = formElement.elements['book-genre'].value
+        book.bookTitle = formElement.elements['book-title'].value
+        book.bookAuthor = formElement.elements['book-author'].value
+        book.bookYear = formElement.elements['book-year'].value
+        book.bookGenre = formElement.elements['book-genre'].value
 
+        localStorage.setItem("books", JSON.stringify(books))
+        renderBooks()
+
+        // update submit button
         const addBook = document.getElementById("add-book")
-        addBook.classList.toggle("edit-btn")
+        addBook.classList.remove("edit-btn")
         addBook.innerText = "Add Book"
 
-        editRowId = null
+        // exit edit mode
+        editBookId = null
     } else {
-        const rowId = 'row-' + Date.now()
-        const row = document.createElement("tr")
-        row.id = rowId
+        const bookId = 'book-' + Date.now()
+        // const row = document.createElement("tr")
+        // row.id = bookId
 
+        // row.innerHTML =
+        //     `
+        //     <td>${title}</td>
+        //     <td>${author}</td>
+        //     <td>${year}</td>
+        //     <td>${genre}</td>
+        //     <td>
+        //         <button onclick="editRow('${bookId}')">Edit</button>
+        //         <button onclick="deleteRow('${bookId}')">Delete</button>
+        //     </td>
+        // `
 
-        row.innerHTML =
-            `
-        <td>${title}</td>
-        <td>${author}</td>
-        <td>${year}</td>
-        <td>${genre}</td>
-        <td>
-            <button onclick="editRow('${rowId}')">Edit</button>
-            <button onclick="deleteRow('${rowId}')">Delete</button>
-        </td>
-        `
-        booksTable.appendChild(row)
+        const newBook = {
+            id: bookId,
+            bookTitle: title,
+            bookAuthor: author,
+            bookYear: year,
+            bookGenre: genre
+        }
+
+        books.push(newBook)
+
+        localStorage.setItem("books", JSON.stringify(books))
+        renderBooks()
+
+        // booksTable.appendChild(row)
     }
-    
+
     formElement.reset()
 })
 
-function deleteRow(rowId) {
-    document.getElementById(rowId).remove()
+
+function deleteRow(bookId) {
+    books = books.filter(book => book.id !== bookId)
+    localStorage.setItem("books", JSON.stringify(books))
+    renderBooks()
+
+    // document.getElementById(bookId).remove()
 }
 
-function editRow(rowId) {
-    const row = document.getElementById(rowId)
+function editRow(bookId) {
+    const book = books.find(book => book.id === bookId)
 
-    const cells = row.querySelectorAll('td')
-    console.log(cells);
+    formElement.elements['book-title'].value = book.bookTitle
+    formElement.elements['book-author'].value = book.bookAuthor
+    formElement.elements['book-year'].value = book.bookYear
+    formElement.elements['book-genre'].value = book.bookGenre
 
-    formElement.elements['book-title'].value = cells[0].innerText
-    formElement.elements['book-author'].value = cells[1].innerText
-    formElement.elements['book-year'].value = cells[2].innerText
-    formElement.elements['book-genre'].value = cells[3].innerText
-
-    editRowId = rowId
+    editBookId = bookId
 
     const addBook = document.getElementById("add-book")
-    addBook.classList.toggle("edit-btn")
+    addBook.classList.add("edit-btn")
     addBook.innerText = "Update Book"
-
 }
